42d1460009d3257d120cb12c694986ec85ba37d2
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-11-26 20:30:42 -0300

    Add project files.

---
+++ Data/Migrations/20221126225613_Tasks.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD.Data.Migrations
+{
+    public partial class Tasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TaskTitle",
+                table: "Task",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TaskComment",
+                table: "Task",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskTitle",
+                table: "Task",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskComment",
+                table: "Task",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ Models/Tasks.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskComment { get; set; }
+
+        public Tasks()
+        {
+
+        }
+    }
+}
